285b0dae3f95165045ca12a419f1783dde7b3ae2
Astghik Simonyan <astghiksimonyan56@gmail.com>
2023-06-14 18:35:50 +0400

    feat: adding html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const { resolve } = require('path')
 
 const config = {
-    devtool: isProd ? false : '#cheap-module-source-map',
 
     entry: {
         app: resolve(__dirname, '..', 'src','js', 'main.js'),
@@ -11,11 +10,7 @@ const config = {
 
     output: {
         path: resolve(__dirname, '..', 'dist'),
-
-        // In our case we serve assets directly from root
         publicPath: '/',
-
-        // We add hash to filename to avoid caching issues
         filename: '[name].[hash].js',
     },
 
@@ -31,9 +26,6 @@ const config = {
             {
               test: /\.js$/,
               loader: 'babel-loader',
-
-              // Dependencies do not require transpilation
-              exclude: /node_modules/
             },
         ],
     },
